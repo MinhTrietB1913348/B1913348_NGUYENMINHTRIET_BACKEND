2b1109eeb5dd85bde769d3caf5b9f181dac95c0c
Triết Nguyễn Minh <trietnguyen020@gmail.com>
2023-01-10 22:49:59 +0700

    Dinh nghia cac route cho tai nguyen contact

---
+++ app/controller/contact.controller.js
@@ -0,0 +1,29 @@
+exports.create=(rea,res)=>{
+    res.send({message:"create handler"});
+};
+
+exports.findAll=(rea,res)=>{
+    res.send({message:"findAll handler"});
+};
+
+exports.findOne=(rea,res)=>{
+    res.send({message:"findOne handler"});
+};
+
+exports.update=(rea,res)=>{
+    res.send({message:"update handler"});
+};
+
+exports.delete=(rea,res)=>{
+    res.send({message:"delete handler"});
+};
+
+exports.deleteAll=(rea,res)=>{
+    res.send({message:"deleteAll handler"});
+};
+
+exports.findAllFavorite=(rea,res)=>{
+    res.send({message:"findAllFavorite handler"});
+};
+
+
